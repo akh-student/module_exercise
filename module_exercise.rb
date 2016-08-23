28ca1709227c007dd7963582b29386c4e5ee34b4
Alyssa Hursh <akhursh@gmail.com>
2016-08-23 10:24:34 -0700

    uncommented a few lines

--- module_exercise.rb
+++ module_exercise.rb
@@ -11,13 +11,13 @@
 ###################################
 
   #
-  # module Perimeter
-  #   class Array
-  #     def initialize
-  #       @size = 400
-  #     end
-  #   end
-  # end
+  module Perimeter
+    class Array
+      def initialize
+        @size = 400
+      end
+    end
+  end
   #
   # our_array = Perimeter::Array.new
   # ruby_array = Array.new
